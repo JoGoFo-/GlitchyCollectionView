947917ca652e9325cd02bd4c374a71ebea8f3f7f
Jonathon Francis <jogofo@gmail.com>
2022-09-26 20:14:29 +1000

    Remove unrelated target offset method

--- GlitchyCollectionView/ScrollingCardCollectionViewLayout.swift
+++ GlitchyCollectionView/ScrollingCardCollectionViewLayout.swift
@@ -35,14 +35,4 @@ class ScrollingCardCollectionViewLayout: UICollectionViewCompositionalLayout {
         let attrs = super.layoutAttributesForItem(at: indexPath)
         return attrs
     }
-    
-    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-        // A minor tweak so that the card 'snaps' to the bottom if the doesn't quite scroll all the way, ensuring all content behind is displayed
-        var point = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
-        guard let scrollView = collectionView else { return point }
-        if velocity.y < 0 && point.y <= -scrollView.adjustedContentInset.top + 250 {
-            point.y = -scrollView.adjustedContentInset.top
-        }
-        return point
-    }
 }
