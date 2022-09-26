c2c4002d0882d887fad11111f41a3cf0936007e3
Jonathon Francis <jogofo@gmail.com>
2022-09-26 16:43:17 +1000

    Initial Commit

---
+++ GlitchyCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlitchyCollectionView
+//
+//  Created by Jonathon Francis on 26/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
